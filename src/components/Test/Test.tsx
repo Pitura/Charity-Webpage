735078473b4fbbb803e272c4594b7f45839231b1
Pitura <pa.pitura@gmail.com>
2022-12-15 23:58:03 +0100

    initial commit

---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import style from './Test.module.scss';
+
+const Test:FC = () => {
+   return (
+      <div className={style.test}>
+         Test
+      </div>
+   );
+}
+ 
+export default Test;
