1ca31840b8435de491bc909fe1cf22ecb76acf68
Pitura <pa.pitura@gmail.com>
2022-12-18 17:26:33 +0100

    login layout

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,9 +1,30 @@
 import { FC } from "react";
+import { CustomHeader } from "../../src/components/common";
+
+import style from "./login.module.scss";
 
 const Login:FC = () => {
    return (
-      <section style={{minHeight:"100vh"}}>
-         Login
+      <section className={style.login_container}>
+         <div className={style.login_box}>
+            <CustomHeader mainText="Zaloguj się"/>
+            <form>
+               <div className={style.inputs_box}>
+                  <div>
+                     <label htmlFor="email">Email</label>
+                     <input type="email" required/>
+                  </div>
+                  <div>
+                     <label htmlFor="password">Hasło</label>
+                     <input type="password" />
+                  </div>
+               </div>
+               <div className={style.button_box}>
+                  <input type="button" value="Załóż konto"/>
+                  <input type="submit" value="Zaloguj się"/>
+               </div>
+            </form>
+         </div>
       </section>
    );
 }
